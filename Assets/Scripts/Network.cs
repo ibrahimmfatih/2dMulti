0e0ef7df90cff8f173aa2c7fe5db9dcba99077b4
ibrahimmfatih <ibrahim.fatih.2002@gmail.com>
2024-08-14 15:56:19 +0300

    First Commit
    Kale Direkleri + Photon giriş aşaması

---
+++ Assets/Scripts/Network.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Network : MonoBehaviourPunCallbacks
+{
+    public string roomName;
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public override void OnConnectedToMaster()
+    {
+        print("Sunucuya Giriþ Yaptýn");
+        JoinOrCreateRoom();
+    }
+   public void JoinOrCreateRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions
+        { MaxPlayers = 10};
+        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, typedLobby: default);
+    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Not In The Room");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("In The Room");
+    
+    Vector3 pos = new Vector3(0, 0, 0);
+    PhotonNetwork.Instantiate("Player" , pos, Quaternion.identity);
+    }
+}
