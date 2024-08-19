e5cd6f6a3e134f70663e776a34e66cbc723cdcbd
ibrahimmfatih <ibrahim.fatih.2002@gmail.com>
2024-08-19 17:52:22 +0300

    sahnelerin entegresi birlikte çalışma durumu sağlandı

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class Ball : MonoBehaviour
+public class Ball : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private int maxScore = 3; // Maximum score to win the game
     [SerializeField] private float force;
     private Rigidbody2D rb;
     [SerializeField] public Score opScore, score;
     [SerializeField] public GameManager gameManager;
-
 
     private void Awake()
     {
@@ -17,25 +18,50 @@ public class Ball : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("SagKale"))
         {
-            score.Scored();
-            Debug.Log("scored");
-            gameManager.ResetPositionsWithDelay();
+            if (collision.CompareTag("SagKale"))
+            {
+                score.Scored();
+                Debug.Log("scored");
+                CheckForWinner();
+                gameManager.ResetPositions();
 
 
-        }
-        if (collision.CompareTag("SolKale"))
-        {
-            opScore.OpScored();
-            Debug.Log("opScored");
-            gameManager.ResetPositionsWithDelay();
+            }
+            if (collision.CompareTag("SolKale"))
+            {
+                opScore.OpScored();
+                Debug.Log("opScored");
+                CheckForWinner();
+                gameManager.ResetPositions();
+            }
         }
     }
-
-    public void Shoot(Vector3 direction)
+        public void Shoot(Vector3 direction)
     {
+        // Sahiplik zaten 'PlayerMovement' sýnýfýnda topa dokunurken devralýndýðý için burada tekrar devralmaya gerek yok.
+
+        rb.velocity = Vector2.zero; // Hýzý sýfýrla, böylece yeni kuvvet doðru þekilde uygulanýr
         rb.AddForce(direction * force);
 
+        // Diðer oyunculara vurma iþlemini bildir
+        photonView.RPC("RPC_Shoot", RpcTarget.Others, direction);
+    }
+    [PunRPC]
+    void RPC_Shoot(Vector3 direction)
+    {
+        rb.velocity = Vector2.zero;  // Kuvvet uygulanmadan önce hýzý sýfýrla
+        rb.AddForce(direction * force);
+    }
+    private void CheckForWinner()
+    {
+        if (score.GetScore() >= maxScore)
+        {
+            gameManager.EndGame("Team 1 Wins!");
+        }
+        else if (opScore.GetOpScore() >= maxScore)
+        {
+            gameManager.EndGame("Team 2 Wins!");
+        }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,43 +9,66 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     private Rigidbody2D rb;
     private Vector3 input;
-
+  
     
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-
-
-      
-
     }
+
     private void Update()
     {
-
-
-        input.x = Input.GetAxis("Horizontal");
-        input.y = Input.GetAxis("Vertical");
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (GetComponent<PhotonView>().IsMine)
         {
-            Collider2D[] colls = Physics2D.OverlapCircleAll(transform.position, radius);
-            foreach(Collider2D coll in colls)
+            input.x = Input.GetAxis("Horizontal");
+            input.y = Input.GetAxis("Vertical");
+
+           
+
+
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                if(coll.TryGetComponent(out Ball ball))
+                Collider2D[] colls = Physics2D.OverlapCircleAll(transform.position, radius);
+                foreach (Collider2D coll in colls)
                 {
-                    Vector3 dir = coll.transform.position - transform.position;
-                    ball.Shoot(dir);
-                    break;
+                    if (coll.TryGetComponent(out Ball ball))
+                    {
+                      
+
+                        Vector3 dir = coll.transform.position - transform.position;
+                        ball.Shoot(dir);
+                        break;
+                    }
                 }
             }
         }
     }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = input * speed;
+
+        // Sadece top hareketsizken veya sahiplik gerçekten gerekli olduðunda sahipliði talep et
+        if (GetComponent<PhotonView>().IsMine)
+        {
+            Collider2D[] colls = Physics2D.OverlapCircleAll(transform.position, radius);
+            foreach (Collider2D coll in colls)
+            {
+                if (coll.TryGetComponent(out Ball ball))
+                {
+                    if (!ball.GetComponent<PhotonView>().IsMine && ball.GetComponent<Rigidbody2D>().velocity.magnitude == 0)
+                    {
+                        ball.GetComponent<PhotonView>().RequestOwnership();
+                    }
+                }
+            }
+        }
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, radius);
-    }
-    private void FixedUpdate()
-    {
-        rb.velocity = input * speed;
     }
 }
