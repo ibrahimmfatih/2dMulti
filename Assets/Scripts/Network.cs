c653b927aba31fcfef6382ffe6b626161e03b7c9
ibrahimmfatih <ibrahim.fatih.2002@gmail.com>
2024-08-20 13:44:46 +0300

    SpawnPoint

--- Assets/Scripts/Network.cs
+++ Assets/Scripts/Network.cs
@@ -6,22 +6,30 @@ using Photon.Realtime;
 
 public class Network : MonoBehaviourPunCallbacks
 {
-   
+    public List<Vector3> blueTeamSpawnPoints; // Mavi takým doðma noktalarý
+    public List<Vector3> redTeamSpawnPoints;  // Kýrmýzý takým doðma noktalarý
+
+    private int blueTeamIndex = 0; // Mavi takým için doðma noktasý sýrasý
+    private int redTeamIndex = 0;  // Kýrmýzý takým için doðma noktasý sýrasý
+
     private void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
     }
+
     public override void OnConnectedToMaster()
     {
         print("Sunucuya Giriþ Yaptýn");
         JoinOrCreateRoom();
     }
-   public void JoinOrCreateRoom()
+
+    public void JoinOrCreateRoom()
     {
         RoomOptions roomOptions = new RoomOptions
-        { MaxPlayers = 10};
+        { MaxPlayers = 10 };
         PhotonNetwork.JoinOrCreateRoom(PlayerPrefs.GetString("room"), roomOptions, typedLobby: default);
     }
+
     public override void OnJoinRoomFailed(short returnCode, string message)
     {
         Debug.Log("Not In The Room");
@@ -30,8 +38,23 @@ public class Network : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         Debug.Log("In The Room");
-    
-    Vector3 pos = new Vector3(0, 0, 0);
-    PhotonNetwork.Instantiate(PlayerPrefs.GetString("player") , pos, Quaternion.identity);
+
+        string playerTeam = PlayerPrefs.GetString("player");
+        Vector3 spawnPos = Vector3.zero;
+
+        if (playerTeam == "PlayerBlue")
+        {
+            // Mavi takým için sýradaki pozisyonu seç
+            spawnPos = blueTeamSpawnPoints[blueTeamIndex];
+            blueTeamIndex = (blueTeamIndex + 1) % blueTeamSpawnPoints.Count; // Bir sonraki indexe geç
+        }
+        else if (playerTeam == "PlayerRed")
+        {
+            // Kýrmýzý takým için sýradaki pozisyonu seç
+            spawnPos = redTeamSpawnPoints[redTeamIndex];
+            redTeamIndex = (redTeamIndex + 1) % redTeamSpawnPoints.Count; // Bir sonraki indexe geç
+        }
+
+        PhotonNetwork.Instantiate(playerTeam, spawnPos, Quaternion.identity);
     }
 }
