3dff757b449051f5258d3577516635ecfd324742
eggamen <egemen.sarak@hotmail.com>
2024-08-20 16:09:38 +0300

    restart button
    restart tuşu ve gol sonrası pozisyon reset

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public List<Vector3> team1SpawnPoints; // Set these in the inspector
+    public List<Vector3> team2SpawnPoints; // Set these in the inspector
+
     public GameObject gameOverPanel; // UI Panel to show when the game ends
     public Text winnerText; // Text to show the winner
 
@@ -16,8 +19,9 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         ballStartPos = ball.transform.position;
-
         gameOverPanel.SetActive(false);
+
+
     }
 
     // Update is called once per frame
@@ -28,10 +32,27 @@ public class GameManager : MonoBehaviour
 
     public void ResetPositions()
     {
-        // Reset the ball and players to their starting positions
+        // Reset the ball position
         ball.transform.position = ballStartPos;
         ball.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        
+
+        // Reset positions of all Team1 (PlayerBlue) players
+        GameObject[] team1Players = GameObject.FindGameObjectsWithTag("Team1");
+        for (int i = 0; i < team1Players.Length; i++)
+        {
+            int spawnIndex = i % team1SpawnPoints.Count;
+            team1Players[i].transform.position = team1SpawnPoints[spawnIndex];
+            team1Players[i].GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+
+        // Reset positions of all Team2 (PlayerRed) players
+        GameObject[] team2Players = GameObject.FindGameObjectsWithTag("Team2");
+        for (int i = 0; i < team2Players.Length; i++)
+        {
+            int spawnIndex = i % team2SpawnPoints.Count;
+            team2Players[i].transform.position = team2SpawnPoints[spawnIndex];
+            team2Players[i].GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
     }
     public void EndGame(string winner)
     {
@@ -42,11 +63,13 @@ public class GameManager : MonoBehaviour
 
     public void RestartGame()
     {
+        PhotonNetwork.Disconnect();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void QuitToMainMenu()
     {
-        SceneManager.LoadScene("MainMenu"); // Replace with your main menu scene name
+        PhotonNetwork.Disconnect();
+        SceneManager.LoadScene("Menu"); // Replace with your main menu scene name
     }
 }
