2a69b59d9240b48a525662068b4491541be4b71d
ibrahimmfatih <ibrahim.fatih.2002@gmail.com>
2024-08-21 20:38:14 +0300

    Kullanıcı Adı Ana Sahnede Görünür Hale Geldi

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,30 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviourPun
 {
     [SerializeField] private float radius;
     [SerializeField] private float speed;
     private Rigidbody2D rb;
     private Vector3 input;
-  
-    
+
+    public TextMeshProUGUI playerNameText;  // TMP Text bileþeni
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        // Kendi oyuncunuz için kullanýcý adýný ayarla ve tüm oyuncularla paylaþ
+        if (photonView.IsMine)
+        {
+            photonView.RPC("SetPlayerName", RpcTarget.AllBuffered, PhotonNetwork.NickName);
+        }
+    }
+
+    [PunRPC]
+    public void SetPlayerName(string name)
+    {
+        playerNameText.text = name;
     }
 
     private void Update()
     {
-        if (GetComponent<PhotonView>().IsMine)
+        if (photonView.IsMine)
         {
             input.x = Input.GetAxis("Horizontal");
             input.y = Input.GetAxis("Vertical");
-
-           
-
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
@@ -34,8 +44,6 @@ public class PlayerMovement : MonoBehaviour
                 {
                     if (coll.TryGetComponent(out Ball ball))
                     {
-                      
-
                         Vector3 dir = coll.transform.position - transform.position;
                         ball.Shoot(dir);
                         break;
@@ -50,7 +58,7 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = input * speed;
 
         // Sadece top hareketsizken veya sahiplik gerçekten gerekli olduðunda sahipliði talep et
-        if (GetComponent<PhotonView>().IsMine)
+        if (photonView.IsMine)
         {
             Collider2D[] colls = Physics2D.OverlapCircleAll(transform.position, radius);
             foreach (Collider2D coll in colls)
